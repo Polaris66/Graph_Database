9e8f59837dbc73d4d0354e69b641f14ae4e41ab1
mssrprad <f20210367@hyderabad.bits-pilani.ac.in>
2023-11-13 15:35:11 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# OS Group Assignment - 2
+
+## Team Members:
+
+#### Add your names here:
+
+1. Malladi Pradyumna
+
+## Setup Instructions:
+
+```
+// Add them here
+```
---
+++ secondary_server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    while (true)
+    {
+        printf("Secondary Server Running 🔥🔥🔥\n");
+        char input;
+        scanf("%c", &input);
+    }
+    return 0;
+}
