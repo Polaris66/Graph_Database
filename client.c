1087cd7283cc48da3506a651fd63bad8004ea9bb
NOOBBITS <97427342+NOOBBITS@users.noreply.github.com>
2023-11-18 00:54:00 +0530

    Updated client.c
    Fixed issues
    Using seq no as unique id+ have to work on message receive

--- client.c
+++ client.c
@@ -15,6 +15,19 @@ typedef struct message_buffer
     char text[100];
 } message;
 
+struct Payload
+{
+    int sequenceNumber;  // Request Number; Unique for each client request
+    int operationNumber; // Operation Number
+    char payload[256];   // Graph FileName
+};
+
+// Message Structure
+typedef struct Message
+{
+    long MessageType;
+    struct Payload payload;
+} Message;
 
 int main() {
     long client_id;
@@ -26,7 +39,7 @@ int main() {
 
     // Get Shared Message Queue
     key = ftok("progfile", 65);
-    key_t shared_memory_key = ftok("shmfile", 65);
+//    key_t shared_memory_key = ftok("shmfile", 65);
     if (msgid == -1)
     {
         perror("Client could not get message queue");
@@ -34,22 +47,11 @@ int main() {
     }
 
     while (1) {
-        message m;
-        strcpy(m.text, "");
+        Message m;
+        //strcpy(m.text, "");
 
         int shared_memory_id;
-        shared_memory_id = shmget(shared_memory_key, SHARED_MEMORY_SIZE, IPC_CREAT | 0666);
-        if(shared_memory_id==-1)
-        {
-            perror("shmget");
-            exit(0);
-        }
-        char *shared_memory = shmat(shared_memory_id, NULL, 0);
-        //CHECK
-        if (shared_memory == (char*)-1) {
-            perror("Error attaching shared memory");
-            exit(EXIT_FAILURE);
-        }     
+
 
     
         // Display menu options
@@ -62,64 +64,36 @@ int main() {
         // scanf("%d", &choice);
 
         // Get user input
-        char sequence_number[3];
-        char operation_number;
+        int sequence_number;
+        int operation_number;
         char graph_file_name[MAX_SIZE];
         printf("Enter Sequence Number: ");
-        scanf("%s", &m.text[0]);
-        // scanf("%d", &sequence_number);
-        if(m.text[1]=='\0')
-       { m.text[1]=' ';
-        m.text[2]=' ';}
-        if(m.text[2]=='\0')
-        m.text[2]=' ';
+        scanf("%s", &m.payload.sequenceNumber);
 
         printf("Enter Operation Number: ");
-        scanf("%s", &m.text[3]);
-        m.text[4]=' ';
+        scanf("%d", &m.payload.operationNumber);
         printf("Enter Graph File Name: ");
-        scanf("%s", &m.text[5]);
-        // printf("%s",)
+        scanf("%s", &m.payload.payload);
 
         // Create a shared memory segment for the request
+        shared_memory_id = shmget(m.payload.sequenceNumber, SHARED_MEMORY_SIZE, IPC_CREAT | 0666);
+        if(shared_memory_id==-1)
+        {
+            perror("shmget");
+            exit(0);
+        }
+        char *shared_memory = shmat(shared_memory_id, NULL, 0);
+        //CHECK
+        if (shared_memory == (char*)-1) {
+            perror("Error attaching shared memory");
+            exit(EXIT_FAILURE);
+        }     
 
+        operation_number=m.payload.operationNumber;
+//        printf("op is %d", operation_number);
 
-        operation_number=m.text[3];
-        // switch (m.text[2])
-        // {
-        // case 1:
-        //     strcpy(m.text, "1");
-        //     int num_nodes;
-        //     printf("Enter number of nodes of the graph: ");
-        //     scanf("%d", &num_nodes);
-        //     int adjacency_matrix[num_nodes][num_nodes];
-
-        //     printf("Enter adjacency matrix: ");
-        //     for(int i=0;i<num_nodes;i++)
-        //     {
-        //         for(int j=0;j<num_nodes;j++)
-        //             scanf("%d", &adjacency_matrix[i][j]);
-        //     }
-
-        //     sprintf(shared_memory, "%d %d", num_nodes, adjacency_matrix);            
-        //     break;
-        // case 2:
-        //     strcpy(m.text, "2");
-        //     break;
-        // case 3:
-        //     strcpy(m.text, "3");
-        //     break;
-        // case 4:
-        //     // Exit Client Process
-        //     exit(0);
-        // default:
-        //     printf("Please Choose a valid option");
-        //     flag = false;
-        //     NL;
-        //     break;
-        // }
         //write operation
-        if (operation_number == '1' || operation_number == '2') {
+        if (operation_number == 1 || operation_number == 2) {
             // For write operations, prompt for additional information
             int num_nodes;
             printf("Enter number of nodes of the graph: ");
@@ -136,16 +110,26 @@ int main() {
                 for (int j = 0; j < num_nodes; j++)
                     scanf("%d", &adjacency_matrix[i][j]);
             }
-            memcpy(shared_memory,adjacency_matrix,sizeof(adjacency_matrix));
+//            memcpy(shared_memory,adjacency_matrix,sizeof(adjacency_matrix));
+//assuming shared memory has adequate space
 
-            sprintf(shared_memory, "%d\n %d", num_nodes, adjacency_matrix);
+            sprintf(shared_memory, "%d", num_nodes);
+            shared_memory+=4;
+            for(int i=0;i<num_nodes;i++)
+            {
+                for(int j=0;j<num_nodes;j++){
+                    sprintf(shared_memory,"%d", adjacency_matrix[i][j]);
+                    shared_memory+=4;
+                }
+
+            }
             for (int i = 0; i < num_nodes; i++) {
                 free(adjacency_matrix[i]);
             }
             free(adjacency_matrix);
         }
 
-        else if (operation_number == '3' || operation_number == '4') {
+        else if (operation_number == 3 || operation_number == 4) {
             // For read operations, prompt for the starting vertex
             int starting_vertex;
             printf("Enter starting vertex: ");
@@ -154,19 +138,6 @@ int main() {
             sprintf(shared_memory, "%d", starting_vertex);
         }
 
-        // Send the request to the load balancer via the message queue
-        // message msg;
-        // msg.type = 1;  // Message type
-        // sprintf(msg.text, "%s %s %s", sequence_number, operation_number, graph_file_name);
-
-
-        // Wait for the server's response
-        // msgrcv(key, &msg, sizeof(msg.text), 2, 0);
-
-        // Process the response (this part needs to be implemented based on your server's response format)
-        // printf("Received response: %s\n", msg.text);
-
-        // Send Message
         int sendRes = msgsnd(msgid, &m, sizeof(m), 0);
 
         // Error Handling
@@ -175,9 +146,26 @@ int main() {
             perror("Message could not be sent by client");
             exit(1);
         }
+        printf("Sent to \"%s\"", &m.payload.payload);
 
-        printf("Sent \"%s\"", &m.text);
+        {
 
+            // Get Reply
+            Message reply;
+            int fetchRes = msgrcv(msgid, &reply, sizeof(reply), 0, 0);
+            //We want to receive BFS/DFS
+            //If msgtyp is equal to zero, the first message on the queue is received.
+            //If msgtyp is greater than 0, the first message of type, msgtyp, is received.
+            //If msgtyp is less than 0, the first message of the lowest type that is less than or equal to the absolute value of msgtyp is received.
+
+            // Error Handling
+            if (fetchRes == -1)
+            {
+                perror("Message could not be recieved by client");
+                exit(1);
+            }
+            printf("Reply from Server: \"%s\"", reply.payload);
+        }
 
         // Detach the shared memory segment
         shmdt(shared_memory);
